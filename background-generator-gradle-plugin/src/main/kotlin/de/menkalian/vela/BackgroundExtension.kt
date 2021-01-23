15c29b02929b277e5319a492c8abf7c040db6b22
Kilian Krampf <kiliankra@gmail.com>
2021-01-23 23:11:02 +0100

    Configurable via gradle buildscript

---
+++ background-generator-gradle-plugin/src/main/kotlin/de/menkalian/vela/BackgroundExtension.kt
@@ -0,0 +1,94 @@
+package de.menkalian.vela
+
+import groovy.lang.Closure
+import org.gradle.api.NamedDomainObjectContainer
+import org.gradle.api.Project
+import org.gradle.api.model.ObjectFactory
+import java.io.File
+import javax.inject.Inject
+import kotlin.math.sqrt
+
+open class BackgroundExtension @Inject constructor(val project: Project, objectFactory: ObjectFactory) {
+    var seed: Long? = null
+    var generationTarget: File = File(project.buildDir, "generated/res/backgrounds/")
+    val backgrounds: NamedDomainObjectContainer<BackgroundConfig> =
+        objectFactory.domainObjectContainer(BackgroundConfig::class.java) { BackgroundConfig(it, project) }
+
+    fun backgrounds(config: NamedDomainObjectContainer<BackgroundConfig>.() -> Unit) = backgrounds.config()
+    fun backgrounds(config: Closure<*>) = project.configure(backgrounds, config)
+}
+
+open class BackgroundConfig(val name: String, val project: Project) {
+    var widthToHeightRatio: Double = 9.0 / 16.0
+    var backgroundBaseColor: String = "#000000"
+
+    var foregroundObjectsAmount: Int = 50
+    var foregroundObjectsShape: String = "star"
+    var foregroundObjectSizeRange: IntRange = 1..5
+    var foregroundObjectColor: String = "#FFFFFF"
+
+    var verticalMovementAnimation: MovementAnimationConfig = MovementAnimationConfig()
+    var horizontalMovementAnimation: MovementAnimationConfig = MovementAnimationConfig()
+    var colorAnimation: ColorAnimationConfig = ColorAnimationConfig()
+    var sizeAnimation: SizeAnimationConfig = SizeAnimationConfig()
+    var opacityAnimation: OpacityAnimationConfig = OpacityAnimationConfig()
+
+    fun verticalMovementAnimation(config: MovementAnimationConfig.() -> Unit) = verticalMovementAnimation.config()
+    fun verticalMovementAnimation(config: Closure<*>): MovementAnimationConfig = project.configure(verticalMovementAnimation, config) as MovementAnimationConfig
+
+    fun horizontalMovementAnimation(config: MovementAnimationConfig.() -> Unit) = horizontalMovementAnimation.config()
+    fun horizontalMovementAnimation(config: Closure<*>): MovementAnimationConfig =
+        project.configure(horizontalMovementAnimation, config) as MovementAnimationConfig
+
+    fun colorAnimation(config: ColorAnimationConfig.() -> Unit) = colorAnimation.config()
+    fun colorAnimation(config: Closure<*>): MovementAnimationConfig = project.configure(colorAnimation, config) as MovementAnimationConfig
+
+    fun sizeAnimation(config: SizeAnimationConfig.() -> Unit) = sizeAnimation.config()
+    fun sizeAnimation(config: Closure<*>): MovementAnimationConfig = project.configure(sizeAnimation, config) as MovementAnimationConfig
+
+    fun opacityAnimation(config: OpacityAnimationConfig.() -> Unit) = opacityAnimation.config()
+    fun opacityAnimation(config: Closure<*>): MovementAnimationConfig = project.configure(opacityAnimation, config) as MovementAnimationConfig
+}
+
+open class MovementAnimationConfig : AnimationConfig() {
+    var direction: Int = 0
+    var speed: Double = 0.0
+}
+
+open class SizeAnimationConfig : AnimationConfig() {
+    var scaleFactor: IntRange = 1..1
+    var interpolator: String = "@android:interpolator/decelerate_cubic"
+}
+
+open class OpacityAnimationConfig : AnimationConfig() {
+    var minValue: Double = 1.0
+    var maxValue: Double = 1.0
+}
+
+open class ColorAnimationConfig : AnimationConfig() {
+    var possibleColors: Array<String> = arrayOf("#FFFFFF")
+
+    companion object {
+        val ALL_COLORS: Array<String>
+        val BRIGHT_COLORS: Array<String>
+
+        init {
+            val listOfAllColorValues = (0x000000..0xFFFFFF).toList()
+            ALL_COLORS = listOfAllColorValues.map { String.format("#%6x", it) }.toTypedArray()
+            BRIGHT_COLORS = listOfAllColorValues
+                .filter {
+                    val rValue = it.shr(16).and(0xFF)
+                    val gValue = it.shr(8).and(0xFF)
+                    val bValue = it.shr(0).and(0xFF)
+                    sqrt(((rValue * rValue) + (gValue * gValue) + (bValue * bValue)).toDouble() / 3 * 255 * 255) > 0.8
+                }
+                .map { String.format("#%6x", it) }.toTypedArray()
+        }
+    }
+}
+
+open class AnimationConfig {
+    var durationRange: IntRange = 300..300
+    var repeat: Boolean = false
+    var repeatMode: String = "reverse"
+}
--- background-generator-gradle-plugin/src/main/kotlin/de/menkalian/vela/BackgroundGradlePlugin.kt
+++ background-generator-gradle-plugin/src/main/kotlin/de/menkalian/vela/BackgroundGradlePlugin.kt
@@ -1,29 +1,32 @@
 package de.menkalian.vela
 
+import com.android.build.api.dsl.ApplicationBaseFlavor
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import java.io.File
+import kotlin.math.roundToInt
 import kotlin.random.Random
 
 class BackgroundGradlePlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        target.pluginManager.withPlugin("com.android.application") {
+        target.extensions.create("backgrounds", BackgroundExtension::class.java, target)
+
+        target.pluginManager.withPlugin("com.android.base") {
             val backgroundGenerator = target.tasks.create("generateBackgroundResources", BackgroundGenerator::class.java)
 
             target.afterEvaluate {
                 target.buildTypeNames().forEach {
                     target.tasks.getByName("generate${it.capitalize()}Resources").dependsOn.add(backgroundGenerator)
                 }
+                val generationBaseDir = target.backgrounds().generationTarget
+                generationBaseDir.mkdirs()
+                (target.extensions.getByName("android") as? CommonExtension<*, *, *, *, *, *, *, *>)!!.sourceSets.getByName("main").res.srcDir(generationBaseDir)
             }
-
-            val generationBaseDir = File(target.buildDir, "generated/res/")
-            generationBaseDir.mkdirs()
-            backgroundGenerator.generationDir = generationBaseDir
-
-            (target.extensions.getByName("android") as? CommonExtension<*, *, *, *, *, *, *, *>)!!.sourceSets.getByName("main").res.srcDir(generationBaseDir)
         }
     }
 }
@@ -32,127 +35,95 @@ internal fun Project.buildTypeNames(): Set<String> =
     ((extensions.getByName("android") as? CommonExtension<*, *, *, *, *, *, *, *>)!!.buildTypes.names)
 
 
+internal fun Project.buildCodeNumber(): Long {
+    return try {
+        ((extensions.getByName("android") as? CommonExtension<*, *, *, *, *, *, *, *>)!!.defaultConfig as ApplicationBaseFlavor<*>).versionCode!!.toLong()
+    } catch (ex: Exception) {
+        Random.nextLong()
+    }
+}
+
+internal fun Project.backgrounds(): BackgroundExtension =
+    extensions.getByName("backgrounds") as BackgroundExtension
+
+
 open class BackgroundGenerator : DefaultTask() {
     lateinit var generationDir: File
 
+    @Input
+    fun getExtensionConfiguration(): BackgroundExtension = project.backgrounds()
+
+    @Input
+    fun getSeed(): Long = project.backgrounds().seed ?: project.buildCodeNumber()
+
+    @OutputDirectory
+    fun getOutputDir(): File = project.backgrounds().generationTarget
+
     @TaskAction
-    fun generateBackground() {
-        val rng = Random.Default
+    fun generateBackgrounds() {
+        val rng = Random(getSeed())
 
-        val listValues = mutableListOf<IntArray>()
+        getExtensionConfiguration().backgrounds.forEach { backgroundConfig ->
+            val listValues = mutableListOf<IntArray>()
 
-        for (i in 1..50) {
-            val posX = rng.nextInt(0, 900)
-            val posY = rng.nextInt(0, 1600)
-            val radius = rng.nextInt(5)
+            val (width: Int, heigth: Int) = getWidthAndHeight(backgroundConfig)
 
-            listValues.add(intArrayOf(posX, posY, radius))
-        }
+            for (i in 1..backgroundConfig.foregroundObjectsAmount) {
+                val posX = rng.nextInt(0, width * 10)
+                val posY = rng.nextInt(0, heigth * 10)
 
-        generateStaticVectorDrawable(listValues)
-        generateAnimatedVectorDrawable(listValues)
-        generateAnimators(listValues, rng)
+                val radius = rng.nextInt(
+                    backgroundConfig.foregroundObjectSizeRange.first,
+                    backgroundConfig.foregroundObjectSizeRange.last
+                )
 
-        logger.info("Generating Colors...")
-        val colorFile = File(generationDir, "values/velaBackgroundColors.xml")
-        colorFile.parentFile.mkdirs()
-        colorFile.createNewFile()
-        colorFile.writeText(
-            "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                    "<resources>\n" +
-                    "    <color name=\"menu_base_background\">#131353</color>\n" +
-                    "    <color name=\"menu_base_stars\">#FFFFC8</color>\n" +
-                    "</resources>"
-        )
-    }
+                listValues.add(intArrayOf(posX, posY, radius))
 
-    private fun generateAnimators(listValues: MutableList<IntArray>, rng: Random.Default) {
-        logger.info("Generating Animators...")
-        val animatorDir = File(generationDir, "animator")
-        animatorDir.mkdirs()
-        listValues.forEachIndexed { index, values ->
-            val animatorFile = File(animatorDir, "star_animator_${String.format("%02d", index)}.xml")
-            animatorFile.createNewFile()
-            animatorFile.writeText(
-                "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                        "<set xmlns:android=\"http://schemas.android.com/apk/res/android\">\n"
-            )
-            animatorFile.appendText(
-                "        <objectAnimator\n" +
-                        "        android:duration=\"${rng.nextInt(20) * 100}\"\n" +
-                        "        android:propertyName=\"strokeWidth\"\n" +
-                        "        android:valueFrom=\"${values[2]}\"\n" +
-                        "        android:valueTo=\"${rng.nextInt(4, 7) * values[2]}\"\n" +
-                        "        android:repeatCount=\"infinite\"\n" +
-                        "        android:repeatMode=\"reverse\" />\n"
-            )
-            animatorFile.appendText("</set>")
+            }
+
+            generateStaticVectorDrawable(listValues, backgroundConfig)
+            generateAnimatedVectorDrawable(listValues, backgroundConfig)
+            generateAnimators(listValues, rng, backgroundConfig)
         }
     }
 
-    private fun generateAnimatedVectorDrawable(listValues: MutableList<IntArray>) {
-        logger.info("Generating AnimatedVectorGraphic...")
-        val animatedBgFile = File(generationDir, "drawable/aquila_star_bg_animated.xml")
-        animatedBgFile.createNewFile()
-        animatedBgFile.writeText(
-            "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                    "<animated-vector xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
-                    "  android:drawable=\"@drawable/aquila_star_bg\" >\n"
-        )
-        listValues.forEachIndexed { index, values ->
-            animatedBgFile.appendText(
-                "<target\n" +
-                        "        android:name=\"star${String.format("%02d", index)}\"\n" +
-                        "        android:animation=\"@animator/star_animator_${String.format("%02d", index)}\" />\n"
-            )
+    private fun getWidthAndHeight(backgroundConfig: BackgroundConfig): Pair<Int, Int> {
+        val width: Int
+        val heigth: Int
+        if (backgroundConfig.widthToHeightRatio > 1) {
+            width = 200
+            heigth = (200 / backgroundConfig.widthToHeightRatio).roundToInt()
+        } else {
+            heigth = 200
+            width = (200 * backgroundConfig.widthToHeightRatio).roundToInt()
         }
-        animatedBgFile.appendText("</animated-vector>")
+        return Pair(width, heigth)
     }
 
-    private fun generateStaticVectorDrawable(listValues: MutableList<IntArray>) {
-        val bgFile = File(generationDir, "drawable/aquila_star_bg.xml")
+    private fun generateStaticVectorDrawable(listValues: MutableList<IntArray>, backgroundConfig: BackgroundConfig) {
+        val (width: Int, heigth: Int) = getWidthAndHeight(backgroundConfig)
+
+        val bgFile = File(generationDir, "drawable/${backgroundConfig.name}.xml")
         bgFile.parentFile.mkdirs()
         bgFile.createNewFile()
         bgFile.writeText(
             "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
                     "<vector xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
-                    "        android:height=\"160dp\"\n" +
-                    "        android:width=\"90dp\"\n" +
-                    "        android:viewportHeight=\"1600\"\n" +
-                    "        android:viewportWidth=\"900\">\n" +
+                    "        android:height=\"${heigth}dp\"\n" +
+                    "        android:width=\"${width}dp\"\n" +
+                    "        android:viewportHeight=\"${heigth * 10}\"\n" +
+                    "        android:viewportWidth=\"${width * 10}\">\n" +
                     "    <path\n" +
-                    "            android:fillColor=\"@color/menu_base_background\"\n" +
+                    "            android:fillColor=\"${backgroundConfig.backgroundBaseColor}\"\n" +
                     "            android:strokeWidth=\"0\"\n" +
-                    "            android:pathData=\"M0,0 900,0 900,1600 0,1600z\" />\n"
+                    "            android:pathData=\"M0,0 ${width * 10},0 ${width * 10},${heigth * 10} 0,${heigth * 10}\" />\n"
         )
         logger.info("Generating VectorGraphic...")
         listValues.forEachIndexed { index, values ->
-            val baseOffset = (1.0 / 3.0) * values[2].toDouble()
-            val extensionOffset = (2.0 / 3.0) * values[2].toDouble()
-
             val xCenter = values[0].toDouble()
-            val xValues = doubleArrayOf(
-                xCenter - baseOffset,
-                xCenter,
-                xCenter + baseOffset,
-                xCenter + baseOffset + extensionOffset,
-                xCenter + baseOffset,
-                xCenter,
-                xCenter - baseOffset,
-                xCenter - baseOffset - extensionOffset
-            )
-
             val yCenter = values[1].toDouble()
-            val yValues = doubleArrayOf(
-                yCenter - baseOffset,
-                yCenter - baseOffset - extensionOffset,
-                yCenter - baseOffset,
-                yCenter,
-                yCenter + baseOffset,
-                yCenter + baseOffset + extensionOffset,
-                yCenter + baseOffset,
-                yCenter
-            )
+
+            val (xValues, yValues) = buildValueData(backgroundConfig.foregroundObjectsShape, xCenter, yCenter, values[2])
 
             val pathData = StringBuilder()
             pathData.append("M")
@@ -165,13 +136,114 @@ open class BackgroundGenerator : DefaultTask() {
 
             bgFile.appendText(
                 "        <path\n" +
-                        "        android:name=\"star${String.format("%02d", index)}\"\n" +
-                        "        android:strokeColor=\"@color/menu_base_stars\"\n" +
+                        "        android:name=\"foreground_${backgroundConfig.name}_${String.format("%02d", index)}\"\n" +
+                        "        android:strokeColor=\"${backgroundConfig.foregroundObjectColor}\"\n" +
                         "        android:strokeWidth=\"${values[2]}\"\n" +
                         "        android:pathData=\"$pathData\"/>\n"
             )
         }
 
         bgFile.appendText("</vector>")
+    }
+
+    private fun buildValueData(shape: String, xCenter: Double, yCenter: Double, size: Int): Pair<DoubleArray, DoubleArray> {
+        val xValues: DoubleArray
+        val yValues: DoubleArray
+        when (shape) {
+            "star"          -> {
+                val baseOffset = (1.0 / 3.0) * size.toDouble()
+
+                xValues = doubleArrayOf(
+                    xCenter - baseOffset,
+                    xCenter,
+                    xCenter + baseOffset,
+                    xCenter + size,
+                    xCenter + baseOffset,
+                    xCenter,
+                    xCenter - baseOffset,
+                    xCenter - size
+                )
+
+                yValues = doubleArrayOf(
+                    yCenter - baseOffset,
+                    yCenter - size,
+                    yCenter - baseOffset,
+                    yCenter,
+                    yCenter + baseOffset,
+                    yCenter + size,
+                    yCenter + baseOffset,
+                    yCenter
+                )
+            }
+
+            else /*square*/ -> {
+                val baseOffset = (1.0 / 3.0) * size.toDouble()
+
+                xValues = doubleArrayOf(
+                    xCenter - baseOffset,
+                    xCenter + baseOffset,
+                    xCenter + baseOffset,
+                    xCenter - baseOffset
+                )
+
+                yValues = doubleArrayOf(
+                    yCenter - baseOffset,
+                    yCenter - baseOffset,
+                    yCenter + baseOffset,
+                    yCenter + baseOffset
+                )
+            }
+        }
+        return Pair(xValues, yValues)
+    }
+
+    private fun generateAnimatedVectorDrawable(listValues: MutableList<IntArray>, backgroundConfig: BackgroundConfig) {
+        logger.info("Generating AnimatedVectorGraphic...")
+        val animatedBgFile = File(generationDir, "drawable/${backgroundConfig.name}_animated.xml")
+        animatedBgFile.createNewFile()
+        animatedBgFile.writeText(
+            "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                    "<animated-vector xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
+                    "  android:drawable=\"@drawable/${backgroundConfig.name}\" >\n"
+        )
+        listValues.forEachIndexed { index, values ->
+            animatedBgFile.appendText(
+                "<target\n" +
+                        "        android:name=\"foreground_${backgroundConfig.name}_${String.format("%02d", index)}\"\n" +
+                        "        android:animation=\"@animator/${backgroundConfig.name}_animator_${String.format("%02d", index)}\" />\n"
+            )
+        }
+        animatedBgFile.appendText("</animated-vector>")
+    }
+
+    private fun generateAnimators(listValues: MutableList<IntArray>, rng: Random, backgroundConfig: BackgroundConfig) {
+        logger.info("Generating Animators...")
+        val animatorDir = File(generationDir, "animator")
+        animatorDir.mkdirs()
+
+        listValues.forEachIndexed { index, values ->
+            val animatorFile = File(animatorDir, "${backgroundConfig.name}_animator_${String.format("%02d", index)}.xml")
+            animatorFile.createNewFile()
+            animatorFile.writeText(
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                        "<set xmlns:android=\"http://schemas.android.com/apk/res/android\">\n"
+            )
+
+            // add Size Animator
+            val sizeConfig = backgroundConfig.sizeAnimation
+            animatorFile.appendText(
+                "        <objectAnimator\n" +
+                        "        android:duration=\"${rng.nextInt(sizeConfig.durationRange.first, sizeConfig.durationRange.last)}\"\n" +
+                        "        android:propertyName=\"strokeWidth\"\n" +
+                        "        android:interpolator=\"${sizeConfig.interpolator}\"\n" +
+                        "        android:valueFrom=\"${values[2]}\"\n" +
+                        "        android:valueTo=\"${rng.nextInt(sizeConfig.scaleFactor.first, sizeConfig.scaleFactor.last) * values[2]}\"\n" +
+                        "        android:valueType=\"floatType\"\n" +
+                        "        android:repeatCount=\"${if (sizeConfig.repeat) "infinite" else "0"}\"\n" +
+                        "        android:repeatMode=\"${sizeConfig.repeatMode}\" />\n"
+            )
+
+            animatorFile.appendText("</set>")
+        }
     }
 }
