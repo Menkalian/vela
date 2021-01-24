7a204afac153aad4ea9a25cd0869164c168fb979
Kilian Krampf <kiliankra@gmail.com>
2021-01-24 02:58:23 +0100

    Add hash-codes

--- background-generator-gradle-plugin/src/main/kotlin/de/menkalian/vela/BackgroundExtension.kt
+++ background-generator-gradle-plugin/src/main/kotlin/de/menkalian/vela/BackgroundExtension.kt
@@ -1,3 +1,5 @@
+@file:Suppress("EqualsOrHashCode")
+
 package de.menkalian.vela
 
 import groovy.lang.Closure
@@ -6,16 +8,23 @@ import org.gradle.api.Project
 import org.gradle.api.model.ObjectFactory
 import java.io.File
 import javax.inject.Inject
-import kotlin.math.sqrt
 
 open class BackgroundExtension @Inject constructor(val project: Project, objectFactory: ObjectFactory) {
     var seed: Long? = null
     var generationTarget: File = File(project.buildDir, "generated/res/backgrounds/")
     val backgrounds: NamedDomainObjectContainer<BackgroundConfig> =
-        objectFactory.domainObjectContainer(BackgroundConfig::class.java) { BackgroundConfig(it, project) }
+        project.container(BackgroundConfig::class.java) { name -> BackgroundConfig(name, project) }
 
     fun backgrounds(config: NamedDomainObjectContainer<BackgroundConfig>.() -> Unit) = backgrounds.config()
     fun backgrounds(config: Closure<*>) = project.configure(backgrounds, config)
+
+    override fun hashCode(): Int {
+        var result = project.hashCode()
+        result = 31 * result + (seed?.hashCode() ?: 0)
+        result = 31 * result + generationTarget.hashCode()
+        result = 31 * result + backgrounds.asMap.values.hashCode()
+        return result
+    }
 }
 
 open class BackgroundConfig(val name: String, val project: Project) {
@@ -48,42 +57,71 @@ open class BackgroundConfig(val name: String, val project: Project) {
 
     fun opacityAnimation(config: OpacityAnimationConfig.() -> Unit) = opacityAnimation.config()
     fun opacityAnimation(config: Closure<*>): MovementAnimationConfig = project.configure(opacityAnimation, config) as MovementAnimationConfig
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + project.hashCode()
+        result = 31 * result + widthToHeightRatio.hashCode()
+        result = 31 * result + backgroundBaseColor.hashCode()
+        result = 31 * result + foregroundObjectsAmount
+        result = 31 * result + foregroundObjectsShape.hashCode()
+        result = 31 * result + foregroundObjectsSizeRange.hashCode()
+        result = 31 * result + foregroundObjectsColor.hashCode()
+        result = 31 * result + verticalMovementAnimation.hashCode()
+        result = 31 * result + horizontalMovementAnimation.hashCode()
+        result = 31 * result + colorAnimation.hashCode()
+        result = 31 * result + sizeAnimation.hashCode()
+        result = 31 * result + opacityAnimation.hashCode()
+        return result
+    }
+
+
 }
 
 open class MovementAnimationConfig : AnimationConfig() {
     var direction: Int = 0
     var speed: Double = 0.0
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + direction
+        result = 31 * result + speed.hashCode()
+        return result
+    }
 }
 
 open class SizeAnimationConfig : AnimationConfig() {
     var scaleFactor: IntRange = 1..1
     var interpolator: String = "@android:interpolator/decelerate_cubic"
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + scaleFactor.hashCode()
+        result = 31 * result + interpolator.hashCode()
+        return result
+    }
 }
 
 open class OpacityAnimationConfig : AnimationConfig() {
     var minValue: Double = 1.0
     var maxValue: Double = 1.0
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + minValue.hashCode()
+        result = 31 * result + maxValue.hashCode()
+        return result
+    }
+
 }
 
 open class ColorAnimationConfig : AnimationConfig() {
     var possibleColors: Array<String> = arrayOf("#FFFFFF")
 
-    companion object {
-        val ALL_COLORS: Array<String>
-        val BRIGHT_COLORS: Array<String>
-
-        init {
-            val listOfAllColorValues = (0x000000..0xFFFFFF).toList()
-            ALL_COLORS = listOfAllColorValues.map { String.format("#%6x", it) }.toTypedArray()
-            BRIGHT_COLORS = listOfAllColorValues
-                .filter {
-                    val rValue = it.shr(16).and(0xFF)
-                    val gValue = it.shr(8).and(0xFF)
-                    val bValue = it.shr(0).and(0xFF)
-                    sqrt(((rValue * rValue) + (gValue * gValue) + (bValue * bValue)).toDouble() / 3 * 255 * 255) > 0.8
-                }
-                .map { String.format("#%6x", it) }.toTypedArray()
-        }
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + possibleColors.contentHashCode()
+        return result
     }
 }
 
@@ -91,4 +129,11 @@ open class AnimationConfig {
     var durationRange: IntRange = 300..300
     var repeat: Boolean = false
     var repeatMode: String = "reverse"
+
+    override fun hashCode(): Int {
+        var result = durationRange.hashCode()
+        result = 31 * result + repeat.hashCode()
+        result = 31 * result + repeatMode.hashCode()
+        return result
+    }
 }
