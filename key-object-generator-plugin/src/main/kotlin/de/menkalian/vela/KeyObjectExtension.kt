174b26c8d63be506dcad43416a229d3f29e594e7
Kilian Krampf <kiliankra@gmail.com>
2021-01-26 22:59:29 +0100

    Use templates, refactor code

--- key-object-generator-plugin/src/main/kotlin/de/menkalian/vela/KeyObjectExtension.kt
+++ key-object-generator-plugin/src/main/kotlin/de/menkalian/vela/KeyObjectExtension.kt
@@ -2,10 +2,16 @@ package de.menkalian.vela
 
 import org.gradle.api.Project
 import java.io.File
+import java.net.URI
 
 open class KeyObjectExtension(project: Project) {
-    var targetDir = File(project.buildDir, "generated/keyobjects")
-    var baseDir = File(project.rootDir, "keys")
+    var targetDir: URI = File(project.buildDir, "generated/vela/keyobject/java").toURI()
+    var sourceDir: URI = File(project.projectDir, "src/main/ckf").toURI()
+
+    var separator = "."
+    var targetPackage = "de.menkalian.vela.generated"
+
     var scanRecursive = false
     var prefixRecursive = false
+    var finalLayerAsString = false
 }
--- key-object-generator-plugin/src/main/kotlin/de/menkalian/vela/KeyObjectGradlePlugin.kt
+++ key-object-generator-plugin/src/main/kotlin/de/menkalian/vela/KeyObjectGradlePlugin.kt
@@ -1,46 +1,34 @@
 package de.menkalian.vela
 
 import com.android.build.api.dsl.CommonExtension
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.node.ArrayNode
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
-import org.gradle.api.DefaultTask
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.InputDirectory
-import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.SourceSetContainer
-import org.gradle.api.tasks.TaskAction
 import java.io.File
 
-const val EXTENSION_NAME = "de.menkalian.vela.keyobjectgen"
+const val EXTENSION_NAME = "keygen"
 
+@Suppress("unused")
 class KeyObjectGradlePlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.extensions.create(EXTENSION_NAME, KeyObjectExtension::class.java, target)
         val generationTask = target.tasks.create("generateKeyObjects", GenerationTask::class.java)
 
         target.pluginManager.withPlugin("java") {
-            target.sourceSets().getByName("main").java.srcDir(target.keyobjectgen().targetDir)
-        }
-        target.pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
-            target.sourceSets().getByName("main").java.srcDir(target.keyobjectgen().targetDir)
+            target.sourceSets().getByName("main").java.srcDir(target.keygenConfig().targetDir)
         }
 
         target.afterEvaluate {
             target.pluginManager.withPlugin("java") {
                 target.tasks.getByName("compileJava").dependsOn(generationTask)
             }
-            target.pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
-                target.tasks.getByName("compileKotlin").dependsOn(generationTask)
-            }
             target.pluginManager.withPlugin("com.android.base") {
                 target.buildTypeNames().forEach {
                     target.tasks.getByName("compile${it.capitalize()}Java").dependsOn.add(generationTask)
                 }
-                val generationBaseDir = target.keyobjectgen().targetDir
+                val generationBaseDir = File(target.keygenConfig().targetDir)
                 generationBaseDir.mkdirs()
                 (target.extensions.getByName("android") as? CommonExtension<*, *, *, *, *, *, *, *>)!!.sourceSets.getByName("main").java.srcDir(
                     generationBaseDir
@@ -56,141 +44,6 @@ internal fun Project.buildTypeNames(): Set<String> =
 internal fun Project.sourceSets(): NamedDomainObjectContainer<SourceSet> =
     extensions.getByName("sourceSets") as SourceSetContainer
 
-internal fun Project.keyobjectgen(): KeyObjectExtension {
+internal fun Project.keygenConfig(): KeyObjectExtension {
     return extensions.getByName(EXTENSION_NAME) as KeyObjectExtension
-}
-
-open class GenerationTask : DefaultTask() {
-    @OutputDirectory
-    fun getTargetDir(): File = project.keyobjectgen().targetDir
-
-    @InputDirectory
-    fun getInputDir(): File = project.keyobjectgen().baseDir
-
-    @TaskAction
-    fun generateObjects() {
-        generateForFile(project.keyobjectgen().baseDir)
-    }
-
-    private fun generateForFile(base: File) {
-        base.listFiles()?.forEach {
-            val generationFile = File(project.keyobjectgen().targetDir, "de/menkalian/vela/generated/GeneratedKey.java")
-            generationFile.parentFile.mkdirs()
-            generationFile.writeText(
-                """
-                    package de.menkalian.vela.generated;
-                    
-                    public class GeneratedKey {
-                        protected final String BASE;
-                    
-                        GeneratedKey(String key, String parent) {
-                            if(parent == null){
-                                BASE = key;
-                            } else {
-                                BASE = parent + "." + key;
-                            }
-                        }
-                        
-                        @Override
-                        public String toString() {
-                            return BASE;
-                        }
-                    }
-                """.trimIndent()
-            )
-
-            if (it.isDirectory && project.keyobjectgen().scanRecursive) {
-                generateForFile(it)
-            } else {
-                val lines = it.readLines()
-                val transformed = File.createTempFile("vela", ".yml")
-                transformed.deleteOnExit()
-
-                lines.forEach { line ->
-                    if (!line.contains(':') && line.isNotBlank())
-                        transformed.appendText("$line:\n")
-                    else
-                        transformed.appendText("$line\n")
-                }
-                println(transformed.readText())
-
-                val keyTree = YAMLMapper().readTree(transformed)
-                keyTree.fields().forEach {
-                    val keyGenerationFile = File(project.keyobjectgen().targetDir, "de/menkalian/vela/generated/${it.key}Key.java")
-                    keyGenerationFile.createNewFile()
-
-                    keyGenerationFile.writeText(
-                        """
-                        package de.menkalian.vela.generated;
-                    
-                        public class ${it.key}Key extends GeneratedKey {
-                            public static ${it.key}Key ${it.key} = new ${it.key}Key();
-                        
-                            private ${it.key}Key () {
-                                super("${it.key}", null);
-                            }
-
-                    """.trimIndent()
-                    )
-
-                    addObjectsToFile(it.value, keyGenerationFile, it.key)
-
-                    keyGenerationFile.appendText("}")
-                }
-            }
-        }
-    }
-
-    private fun addObjectsToFile(value: JsonNode?, keyGenerationFile: File, parent: String) {
-        if (value == null) return
-
-        if (value.isTextual) {
-            keyGenerationFile.appendText(
-                """
-                    public ${value}Key $value = new ${value}Key();
-                    public static class ${value}Key extends GeneratedKey {
-                   
-                        private ${value}Key () {
-                            super("$value", ${parent}.toString());
-                        }
-                    }
-                    """.trimIndent()
-            )
-            return
-        }
-
-        if (value.isArray) {
-            (value as ArrayNode).forEach {
-                keyGenerationFile.appendText(
-                    """
-                    public ${it.textValue()}Key ${it.textValue()} = new ${it.textValue()}Key();
-                    public static class ${it.textValue()}Key extends GeneratedKey {
-                   
-                        private ${it.textValue()}Key () {
-                            super("${it.textValue()}", ${parent}.toString());
-                        }
-                    }
-                    """.trimIndent()
-                )
-            }
-            return
-        }
-
-        value.fields().forEach {
-            keyGenerationFile.appendText(
-                """
-                public ${it.key}Key ${it.key} = new ${it.key}Key();
-                public static class ${it.key}Key extends GeneratedKey {
-                
-                    private ${it.key}Key () {
-                        super("${it.key}", ${parent}.toString());
-                    }
-                """.trimIndent()
-            )
-
-            addObjectsToFile(it.value, keyGenerationFile, "$parent.${it.key}")
-
-            keyGenerationFile.appendText("}")
-        }
-    }
 }
