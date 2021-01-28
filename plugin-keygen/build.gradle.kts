0883f76b055da23ab32be0aed16b1752c4609337
Kilian Krampf <kiliankra@gmail.com>
2021-01-28 23:41:24 +0100

    Setup JUnit in Buildscript

--- plugin-keygen/build.gradle.kts
+++ plugin-keygen/build.gradle.kts
@@ -21,11 +21,54 @@ gradlePlugin {
 }
 
 dependencies {
-    implementation(jackson("core"))
-    implementation(jackson("databind"))
+    implementation("com.fasterxml.jackson.core:jackson-core:2.12.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.0")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.0")
 
     compileOnly("com.android.tools.build:gradle:4.1.1")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
 }
 
-fun jackson(module: String) = "com.fasterxml.jackson.core:jackson-$module:2.12.0"
+tasks {
+    test {
+        useJUnitPlatform {
+            includeTags("unit")
+        }
+    }
+}
+
+val testSourceSet = sourceSets.getByName("test")
+val integrationTestTask = tasks.register<Test>("integrationTest") {
+    description = "Runs the integration tests."
+    group = "verification"
+    testClassesDirs = testSourceSet.output.classesDirs
+    classpath = testSourceSet.runtimeClasspath
+    mustRunAfter(tasks.test)
+    useJUnitPlatform {
+        includeTags("integration")
+    }
+}
+val functionalTestTask = tasks.register<Test>("functionalTest") {
+    description = "Runs the functional tests."
+    group = "verification"
+    testClassesDirs = testSourceSet.output.classesDirs
+    classpath = testSourceSet.runtimeClasspath
+    mustRunAfter(tasks.test)
+    useJUnitPlatform {
+        includeTags("functional")
+    }
+}
+
+
+tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java) {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+tasks.check {
+    dependsOn(integrationTestTask)
+    dependsOn(functionalTestTask)
+}
