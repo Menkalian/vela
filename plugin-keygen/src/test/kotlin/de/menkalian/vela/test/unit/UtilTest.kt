69aa2639a1838cf72b53dd3eee7028a00a023f89
Kilian Krampf <kiliankra@gmail.com>
2021-01-29 22:46:29 +0100

    Implement Tests for Utils

---
+++ plugin-keygen/src/test/kotlin/de/menkalian/vela/test/unit/UtilTest.kt
@@ -0,0 +1,54 @@
+package de.menkalian.vela.test.unit
+
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
+import de.menkalian.vela.plain.transformToYaml
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+
+@Tag("unit")
+internal class UtilTest {
+
+    @Test
+    fun doesNotTransformValidYAML() {
+        val yaml = """
+            test:
+              value:
+                vela: kalix
+                aquila: menkalian
+                auriga:
+              anotherOne:
+        """.trimIndent()
+        assertEquals(yaml.trim(), transformToYaml(yaml).trim(),"Should not transform valid yaml")
+    }
+
+    @Test
+    fun doesProduceValidYaml() {
+        val input = "vela\n" +
+                "  tests\n" +
+                "    unit\n" +
+                "    integration\n" +
+                "    functional\n" +
+                "  prod\n" +
+                "    plugins\n" +
+                "      versioning\n" +
+                "      keygen\n"
+        assertDoesNotThrow {
+            YAMLMapper().readTree(transformToYaml(input))
+        }
+    }
+
+    @Test
+    fun doesNotAlterExistingYamlObject() {
+        val mapper = YAMLMapper()
+        val initalNode = mapper.createObjectNode()
+        initalNode.set<ObjectNode>(
+            "vela", mapper.createObjectNode()
+                .put("tests", "unit")
+        )
+        val input = mapper.writeValueAsString(initalNode).trim()
+        assertEquals(initalNode, mapper.readTree(transformToYaml(input)), "Should not alter YAML")
+    }
+}
