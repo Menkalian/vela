9379756844141b7953502a5d67c302275a02dbe1
Kilian Krampf <kilian@krampf.de>
2021-02-07 22:36:09 +0100

    Fix output file for buildconfig

--- plugin-buildconfig/src/main/kotlin/de/menkalian/vela/gradle/BuildconfigTask.kt
+++ plugin-buildconfig/src/main/kotlin/de/menkalian/vela/gradle/BuildconfigTask.kt
@@ -4,7 +4,6 @@ import de.menkalian.vela.plain.SourceGenerator
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 
@@ -21,13 +20,12 @@ open class BuildconfigTask : DefaultTask() {
     @OutputDirectory
     val outputDir = File(project.buildDir, "generated/vela/buildconfig/java")
 
-    @OutputFile
-    val outputFile = File(outputDir, "BuildConfig.java")
+    private fun getOutputFile() = File(outputDir, "${projectGroup.replace(".", "/")}/BuildConfig.java")
 
     @TaskAction
     fun generateBuildConfig() {
         SourceGenerator(
-            outputFile,
+            getOutputFile(),
             "BuildConfig",
             mapOf(
                 "group" to projectGroup,
