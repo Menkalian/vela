fc81b447cab404e14505ca8780f07423a12b55e7
Kilian Krampf <kilian@krampf.de>
2021-07-04 12:58:26 +0200

    TOOL-TEMPLATE Implement Test for TextNode

--- tool-template/src/test/kotlin/de/menkalian/vela/template/evaluator/node/LeafNodeTest.kt
+++ tool-template/src/test/kotlin/de/menkalian/vela/template/evaluator/node/LeafNodeTest.kt
@@ -1,10 +1,23 @@
 package de.menkalian.vela.template.evaluator.node
 
+import de.menkalian.vela.template.Variables
+import de.menkalian.vela.template.evaluator.node.leaf.TextNode
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 internal class LeafNodeTest {
     @Test
     fun testTextNode() {
+        val testValue =  { text : String ->
+            val node = TextNode(text)
+            val tmp = StringBuilder()
+            node.evaluate(Variables(), tmp)
+            Assertions.assertEquals(text, tmp.toString())
+            Assertions.assertEquals(text, node.getValue(Variables()))
+        }
 
+        testValue("ujK")
+        testValue("wreck")
+        testValue("9ARPx")
     }
 }
