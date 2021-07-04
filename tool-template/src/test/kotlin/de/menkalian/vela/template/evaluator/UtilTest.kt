b57c3e9dd6eaf346ec11841a43cb3c1c423f6621
Kilian Krampf <kilian@krampf.de>
2021-07-04 12:50:14 +0200

    TOOL-TEMPLATE Implement Test for InterpretAsBooleanHelper

--- tool-template/src/test/kotlin/de/menkalian/vela/template/evaluator/UtilTest.kt
+++ tool-template/src/test/kotlin/de/menkalian/vela/template/evaluator/UtilTest.kt
@@ -1,10 +1,20 @@
 package de.menkalian.vela.template.evaluator
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class UtilTest {
     @Test
     fun testInterpretAsBoolean() {
-
+        Assertions.assertTrue("true".interpretAsBoolean())
+        Assertions.assertTrue("YEP".interpretAsBoolean())
+        Assertions.assertTrue("1".interpretAsBoolean())
+        Assertions.assertFalse("0".interpretAsBoolean())
+        Assertions.assertFalse("false".interpretAsBoolean())
+        Assertions.assertFalse("FALSE".interpretAsBoolean())
+        Assertions.assertFalse("fAlsE".interpretAsBoolean())
+        Assertions.assertTrue("tRue".interpretAsBoolean())
+        Assertions.assertTrue("764".interpretAsBoolean())
+        Assertions.assertTrue("Z026".interpretAsBoolean())
     }
 }
