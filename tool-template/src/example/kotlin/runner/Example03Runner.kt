1ef1d983479edfbbdef740009cc56fb561af949f
Kilian Krampf <kilian@krampf.de>
2021-08-07 00:33:56 +0200

    TOOL-TEMPLATE Write example 3

--- tool-template/src/example/kotlin/runner/Example03Runner.kt
+++ tool-template/src/example/kotlin/runner/Example03Runner.kt
@@ -1,7 +1,28 @@
 package runner
 
+import ResourceHelper
 import printExampleResult
+import java.io.File
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.createTempDirectory
 
+@OptIn(ExperimentalPathApi::class)
 fun main() {
-    printExampleResult("03_Arithmetic_Operations.vtp")
+    val tmpDir = createTempDirectory().toFile()
+    loadResourcePath(tmpDir, "03/hint.vtp")
+    loadResourcePath(tmpDir, "03/SetVariable.vtp")
+    System.setProperty("user.dir", tmpDir.absolutePath)
+
+    printExampleResult("03_include_files.vtp")
+
+    tmpDir.deleteRecursively()
+}
+
+private fun loadResourcePath(targetDir: File, path: String) {
+    val inStream = ResourceHelper::class.java
+        .classLoader.getResourceAsStream(path)!!
+    val target = File(targetDir, path)
+    target.parentFile.mkdirs()
+    target.writeBytes(inStream.readAllBytes())
+    inStream.close()
 }
