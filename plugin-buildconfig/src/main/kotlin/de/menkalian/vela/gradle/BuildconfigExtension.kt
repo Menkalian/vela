6673ee784b28efa114383bc2aeb19cd4c7b58e5a
Kilian Krampf <kilian@krampf.de>
2022-04-08 00:17:12 +0200

    cleanup/improve Buildconfig plugin

---
+++ plugin-buildconfig/src/main/kotlin/de/menkalian/vela/gradle/BuildconfigExtension.kt
@@ -0,0 +1,71 @@
+package de.menkalian.vela.gradle
+
+import de.menkalian.vela.gradle.BuildconfigExtension.Generator.DEFAULT
+import de.menkalian.vela.gradle.BuildconfigExtension.Generator.JAVA
+import de.menkalian.vela.gradle.BuildconfigExtension.Generator.KOTLIN
+import org.gradle.api.Project
+import org.gradle.api.provider.Property
+import java.io.File
+
+/**
+ * Extension to configure the behaviour of the plugin
+ */
+abstract class BuildconfigExtension(project: Project) {
+    companion object {
+        /**
+         * Constant Name which is used to register (and find) the extension
+         */
+        const val EXTENSION_NAME = "buildconfig"
+    }
+
+    /**
+     * Target directory where the generated sources are put.
+     */
+    abstract val targetDir: Property<File>
+
+    /**
+     * Target package for the generated sources
+     */
+    abstract val targetPackage: Property<String>
+
+    /**
+     * Generator type to use
+     */
+    abstract val generator: Property<Generator>
+
+    /**
+     * Properties to put inside the BuildConfig.
+     * The initial value contains three properties: group, name and version of the gradle project
+     */
+    val additionalProperties: MutableMap<String, (Project) -> String> = mutableMapOf()
+
+    init {
+        targetDir.convention(File(project.buildDir.absolutePath + "/generated/vela/buildconfig"))
+        targetPackage.convention(project.group.toString())
+        generator.convention(Generator.DEFAULT)
+
+        additionalProperties["group"] = { it.group.toString() }
+        additionalProperties["module"] = { it.name.toString() }
+        additionalProperties["version"] = { it.version.toString() }
+    }
+
+    /**
+     * Enumeration of the available Generator types
+     *
+     * @property JAVA Generate Java Sources
+     * @property KOTLIN Generate Kotlin Sources
+     * @property DEFAULT Use the type which best matches the current project
+     */
+    enum class Generator {
+        JAVA,
+        KOTLIN,
+        DEFAULT;
+
+        companion object {
+            /**
+             * Property to determine the best fitted generator type for the current project
+             */
+            internal var defaultGenerator: Generator = JAVA
+        }
+    }
+}
--- plugin-buildconfig/src/main/kotlin/de/menkalian/vela/gradle/BuildconfigGradlePlugin.kt
+++ plugin-buildconfig/src/main/kotlin/de/menkalian/vela/gradle/BuildconfigGradlePlugin.kt
@@ -1,16 +1,143 @@
+@file:Suppress("UnstableApiUsage")
+
 package de.menkalian.vela.gradle
 
+import com.android.build.api.dsl.AndroidSourceSet
+import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.configurationcache.extensions.capitalized
+import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.targets
 
+/**
+ * Main class of the plugin
+ */
 open class BuildconfigGradlePlugin : Plugin<Project> {
-    override fun apply(target: Project) {
-        target.tasks.create("generateBuildConfig", BuildconfigTask::class.java)
-    }
-}
 
-internal fun Project.sourceSets(): NamedDomainObjectContainer<SourceSet> =
-    extensions.getByName("sourceSets") as SourceSetContainer
+    /**
+     * Initialization method. Called when the plugin is applied to the project
+     *
+     * @param target Gradle-Project to apply this plugin on
+     */
+    override fun apply(target: Project) {
+        val buildconfigExtension = target.extensions
+            .create(
+                BuildconfigExtension.EXTENSION_NAME,
+                BuildconfigExtension::class.java,
+                target
+            )
+
+        val buildconfigTask = target.tasks
+            .create(
+                "generateBuildConfig",
+                BuildconfigTask::class.java
+            )
+
+
+        target.afterEvaluate {
+            // Determine the best generator type
+            if (target.pluginManager.hasPlugin("org.jetbrains.kotlin.jvm")
+                || target.pluginManager.hasPlugin("org.jetbrains.kotlin.multiplatform")
+                || target.pluginManager.hasPlugin("org.jetbrains.kotlin.android")
+            ) {
+                BuildconfigExtension.Generator.defaultGenerator =
+                    BuildconfigExtension.Generator.KOTLIN
+            } else {
+                BuildconfigExtension.Generator.defaultGenerator =
+                    BuildconfigExtension.Generator.JAVA
+            }
+
+            // Configure compileTasks to run after the generation
+            target.pluginManager.withPlugin("java") {
+                target.tasks.getByName("compileJava").dependsOn(buildconfigTask)
+            }
+            target.pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+                target.tasks.getByName("compileKotlin").dependsOn(buildconfigTask)
+            }
+            target.pluginManager.withPlugin("org.jetbrains.kotlin.multiplatform") {
+                target.tasks.withType(KotlinCompile::class.java)
+                    .configureEach {
+                        it.dependsOn(buildconfigTask)
+                    }
+            }
+            target.pluginManager.withPlugin("com.android.base") {
+                target.androidBuildTypeNames().forEach {
+                    target.tasks.getByName("compile${it.capitalized()}Java").dependsOn(buildconfigTask)
+                }
+            }
+
+            // Add source directories
+            val sourceDirectories = arrayOf(
+                buildconfigExtension.targetDir.get().absolutePath + "/java",
+                buildconfigExtension.targetDir.get().absolutePath + "/kotlin",
+            )
+
+            target.pluginManager.withPlugin("java") {
+                target.sourceSets()
+                    .getByName("main").java
+                    .srcDirs(*sourceDirectories)
+            }
+            target.pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+                target.sourceSets()
+                    .getByName("main").java
+                    .srcDirs(*sourceDirectories)
+            }
+            target.pluginManager.withPlugin("org.jetbrains.kotlin.multiplatform") {
+                target.kotlinCommonCodeSourceSet()?.kotlin
+                    ?.srcDirs(*sourceDirectories)
+            }
+
+            target.pluginManager.withPlugin("com.android.base") {
+                target.androidSourceSets().getByName("main")
+                    .java.srcDirs(*sourceDirectories)
+            }
+        }
+    }
+
+    /**
+     * Utility method to obtain the SourceSetContainer for a project
+     */
+    private fun Project.sourceSets(): NamedDomainObjectContainer<SourceSet> =
+        extensions.getByName("sourceSets") as SourceSetContainer
+
+    /**
+     * Utility method to obtain the SourceSetContainer of the Kotlin (Multiplatform) plugin
+     */
+    private fun Project.kotlinSourceSets(): NamedDomainObjectContainer<KotlinSourceSet> =
+        kotlinExtension.sourceSets
+
+    /**
+     * Utility method to try to find the main shared SourceSet of a Kotlin (Multiplatform) project
+     */
+    private fun Project.kotlinCommonCodeSourceSet(): KotlinSourceSet? {
+        val commonMain = kotlinSourceSets().findByName("commonMain")
+        if (commonMain != null) {
+            return commonMain
+        }
+
+        // Find first common code source set
+        return project.kotlinExtension.targets
+            .firstOrNull {
+                it.platformType == KotlinPlatformType.common
+            }?.compilations?.firstOrNull()?.defaultSourceSet
+    }
+
+    /**
+     * Utility method to obtain the SourceSetContainer of the Android plugin
+     */
+    private fun Project.androidSourceSets(): NamedDomainObjectContainer<out AndroidSourceSet> =
+        (extensions.getByName("android") as? CommonExtension<*, *, *, *>)!!.sourceSets
+
+    /**
+     * Utility method to obtain the BuildTypes of the Android plugin
+     */
+    private fun Project.androidBuildTypeNames(): Set<String> =
+        (extensions.getByName("android") as? CommonExtension<*, *, *, *>)!!.buildTypes.names
+}
