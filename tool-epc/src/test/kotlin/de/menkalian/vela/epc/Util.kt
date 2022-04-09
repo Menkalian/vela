d9403c4b741dfdb51e74a8a81a9b0735a6a81b1e
Kilian Krampf <kilian@krampf.de>
2022-04-09 21:06:14 +0200

    fix failing test (linefeed difference between windows and linux)

--- tool-epc/src/test/kotlin/de/menkalian/vela/epc/Util.kt
+++ tool-epc/src/test/kotlin/de/menkalian/vela/epc/Util.kt
@@ -1,18 +1,24 @@
 package de.menkalian.vela.epc
 
 import java.io.File
-import java.util.Arrays
 
 fun compareFiles(f1: File, f2: File): Boolean {
+    println("Comparing $f1 to $f2")
     if (f1.isDirectory) {
-        return f1.list()?.size == f2.list()?.size ?: false &&
+        return f1.list()?.let { it.size == f2.list()?.size } == true &&
                 f1.list()?.all {
                     compareFiles(File(f1, it), File(f2, it))
-                } ?: false
-
+                } == true
     } else {
         return f1.exists() && f2.exists() &&
-                f1.length() == f2.length() &&
-                Arrays.equals(f1.readBytes(), f2.readBytes())
+                (f1.readBytes().contentEquals(f2.readBytes())
+                        || f1.readText().equalsWithoutLF(f2.readText()))
     }
 }
+
+fun String.equalsWithoutLF(other: String) = this.lineFeed().contentEquals(other.lineFeed())
+
+fun String.lineFeed() = this
+    .replace("\r\n", "\n")
+    .replace("\r", "\n")
+
