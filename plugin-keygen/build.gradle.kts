897a1485ab82f83d5653e5dad47ed8ec329cf334
Kilian Krampf <kilian@krampf.de>
2022-05-30 21:10:28 +0200

    Plugin-Keygen Adapt build.gradle.kts

--- plugin-keygen/build.gradle.kts
+++ plugin-keygen/build.gradle.kts
@@ -1,12 +1,14 @@
 @file:Suppress("SpellCheckingInspection")
 
 plugins {
-    kotlin("jvm")
     `java-gradle-plugin`
-    id("org.jetbrains.dokka")
-    id("com.gradle.plugin-publish")
     `maven-publish`
     jacoco
+
+    kotlin("jvm")
+
+    id("com.gradle.plugin-publish")
+    id("org.jetbrains.dokka")
 }
 
 tasks.test {
@@ -17,7 +19,7 @@ gradlePlugin {
     plugins {
         create("keygenPlugin") {
             id = "de.menkalian.vela.keygen"
-            implementationClass = "de.menkalian.vela.gradle.KeyObjectGradlePlugin"
+            implementationClass = "de.menkalian.vela.gradle.KeygenGradlePlugin"
             displayName = "Key Object Generator Plugin"
             description = ""
         }
@@ -29,7 +31,9 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.12.0")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.0")
 
-    compileOnly("com.android.tools.build:gradle:4.1.1")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    implementation("com.android.tools.build:gradle:7.1.1")
 
+    implementation(project(":tool-template"))
     testImplementation(project(":tool-epc"))
 }
